f6b85b95fa08235bdab61cc94a0ef41558198de8
conkraw <conkraw@gmail.com>
2026-03-02 21:31:47 -0500

    Update app2627.py

--- app2627.py
+++ app2627.py
@@ -516,7 +516,7 @@ elif instrument == "Roster_HMC":
     )
 
     # --------- REMOVE QUIZ DUE COLUMNS COMPLETELY ----------
-    df_roster = (df_roster[['lastname', 'firstname', 'email', 'rotation']].rename(columns={'lastname': 'last_name','firstname': 'first_name'}))
+    df_roster = (df_roster[['record_id','lastname', 'firstname', 'email', 'rotation']].rename(columns={'lastname': 'last_name','firstname': 'first_name'}))
     
     st.download_button("📥 Download roster_intake_form csv",df_roster.to_csv(index=False).encode("utf-8"),file_name="roster_intake_form.csv",mime="text/csv")
 
