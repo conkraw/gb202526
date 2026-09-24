c6a80fb7bb332e72c1dd11a827a44518643214ce
conkraw <conkraw@gmail.com>
2026-09-24 13:33:48 -0400

    Update app2627.py

--- app2627.py
+++ app2627.py
@@ -4440,14 +4440,14 @@ elif instrument == "Session Feedback Summary Creator":
                     )
 
 
-                    #edited_question_config = st.data_editor(question_config,hide_index=True,use_container_width=True,disabled=["REDCap Field"],key=("feedback_summary_", "question_editor"))
+                    edited_question_config = st.data_editor(question_config,hide_index=True,use_container_width=True,disabled=["REDCap Field"],key=("feedback_summary_", "question_editor"))
 
 
                     included_question_rows = (
                         edited_question_config[
                             edited_question_config[
                                 "Include"
-                            ] == True
+                            ] == False
                         ]
                     )
 
