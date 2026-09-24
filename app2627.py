e6b69d0c3b904f66d2cfd75a70ee36c67739d4da
conkraw <conkraw@gmail.com>
2026-09-24 17:19:22 -0400

    Update app2627.py

--- app2627.py
+++ app2627.py
@@ -2811,7 +2811,7 @@ elif instrument == "Session Feedback Link Creator":
 
                             st.rerun()
 
-elif instrument == "Session Feedback Summary":
+elif instrument == "Session Feedback Summary Creator":
     st.header("📋 Session Feedback Summary Creator")
 
     import io
